611442b1f2cb3803be8fbb3e4af78b964e4219c9
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2024-09-02 22:26:40 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+import java.io.ByteArrayOutputStream
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation(project(":ext"))
+    val libVersion: String by project
+    compileOnly("com.github.brahmkshatriya:echo:$libVersion")
+}
+
+val extType: String by project
+val extId: String by project
+val extClass: String by project
+
+val extIconUrl: String? by project
+val extName: String by project
+val extDescription: String? by project
+
+val extAuthor: String by project
+val extAuthorUrl: String? by project
+
+val extRepoUrl: String? by project
+val extUpdateUrl: String? by project
+
+val gitHash = execute("git", "rev-parse", "HEAD").take(7)
+val gitCount = execute("git", "rev-list", "--count", "HEAD").toInt()
+val verCode = gitCount
+val verName = gitHash
+
+android {
+    namespace = "dev.brahmkshatriya.echo.extension"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "dev.brahmkshatriya.echo.extension.$extId"
+        minSdk = 24
+        targetSdk = 35
+
+        manifestPlaceholders.apply {
+            put("type", "dev.brahmkshatriya.echo.${extType}")
+            put("id", extId)
+            put("class_path", "dev.brahmkshatriya.echo.extension.${extClass}")
+            put("version", verName)
+            put("version_code", verCode.toString())
+            put("icon_url", extIconUrl ?: "")
+            put("app_name", "Echo : $extName Extension")
+            put("name", extName)
+            put("description", extDescription?:"")
+            put("author", extAuthor)
+            put("author_url", extAuthorUrl ?: "")
+            put("repo_url", extRepoUrl ?: "")
+            put("update_url", extUpdateUrl ?: "")
+        }
+    }
+
+    buildTypes {
+        all {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+fun execute(vararg command: String): String {
+    val outputStream = ByteArrayOutputStream()
+    project.exec {
+        commandLine(*command)
+        standardOutput = outputStream
+    }
+    return outputStream.toString().trim()
+}
---
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/TestExtension.kt
@@ -0,0 +1,16 @@
+package dev.brahmkshatriya.echo.extension
+
+import dev.brahmkshatriya.echo.common.clients.ExtensionClient
+import dev.brahmkshatriya.echo.common.settings.Setting
+import dev.brahmkshatriya.echo.common.settings.Settings
+
+class TestExtension : ExtensionClient {
+    override suspend fun onExtensionSelected() {}
+
+    override val settingItems: List<Setting> = emptyList()
+
+    private lateinit var setting: Settings
+    override fun setSettings(settings: Settings) {
+        setting = settings
+    }
+}
